d69a58b3b240b21cb41e1f75f7a9dc3bfed686e7
user.name <aselsatybaldieva@gmail.com>
2018-11-04 02:34:37 +0000

    updated

---
+++ efs.tf
@@ -0,0 +1,3 @@
+resource "aws_efs_file_system" "efs1" {
+  creation_token = "production"
+}
---
+++ keys.tf
@@ -0,0 +1,3 @@
+resource "aws_key_pair" "jenkins" {
+ key_name   = "jenkins"
+ public_key = "${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
